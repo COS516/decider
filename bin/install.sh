1a4bfe8cf82629470c9adcf933c6341b1764e053
Daniel Suo <danielsuo@gmail.com>
2017-01-15 08:30:24 -0500

    Update install to include linux

--- bin/install.sh
+++ bin/install.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
-brew install python
-python setup.py develop
 
-cd lib/minisat-os-x
-make config prefix=$(pwd)
-make install
+if [[ "OSTYPE" == "linux-gnu" ]]; then
+    wget http://minisat.se/downloads/minisat-2.2.0.tar.gz
+    tar zxvf minisat*
+    cd minisat
+    export MROOT=$(pwd)
+    cd core
+    # sudo apt-get libghc-zlib-dev
+    make
+    ln -s $pwd/minisat /usr/local/bin/minisat
+    cd ../../
+elif [[ "OSTYPE" == "darwin"* ]]; then
 
-ln -s $(pwd)/lib/libminisat.a /usr/local/lib/libminisat.a
-ln -s $(pwd)/lib/libminisat.so /usr/local/lib/libminisat.so
-ln -s $(pwd)/lib/libminisat.so.2 /usr/local/lib/libminisat.so.2
-ln -s $(pwd)/lib/libminisat.so.2.1.0 /usr/local/lib/libminisat.so.2.1.0
-ln -s $(pwd)/bin/minisat /usr/local/bin/minisat
+    brew install python
+    python setup.py develop
 
-cd ../../
+    cd lib/minisat-os-x
+    make config prefix=$(pwd)
+    make install
+
+    ln -s $(pwd)/lib/libminisat.a /usr/local/lib/libminisat.a
+    ln -s $(pwd)/lib/libminisat.so /usr/local/lib/libminisat.so
+    ln -s $(pwd)/lib/libminisat.so.2 /usr/local/lib/libminisat.so.2
+    ln -s $(pwd)/lib/libminisat.so.2.1.0 /usr/local/lib/libminisat.so.2.1.0
+    ln -s $(pwd)/bin/minisat /usr/local/bin/minisat
+
+    cd ../../
+fi
